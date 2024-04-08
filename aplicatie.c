2c958f65aa7ad1ad8ababe97da31d6746340874a
rauldavid35 <75571303+rauldavid35@users.noreply.github.com>
2024-04-08 11:45:18 +0300

    Update4 aplicatie.c

--- aplicatie.c
+++ aplicatie.c
@@ -1,84 +1,3 @@
-/*#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <string.h>
-#include <time.h>
-#include <errno.h>
-
-void verificareAcelasiContinut(const char *director,DIR *pdir,FILE* fisier){
-    struct dirent *citireDirector=NULL;
-    struct stat informatiiFisier;
-    char timpModificare[30];
-    char *caleDirector=NULL;
-
-    while((citireDirector=readdir(pdir))!=NULL){
-
-        if((caleDirector=(char*)malloc(sizeof(char)*(strlen(director)+strlen(citireDirector->d_name)+2)))==NULL){
-            printf("Eroare alocare memorie pentru cale!!!\n");
-            exit(-1);
-        }
-        strcpy(caleDirector,director);
-        strcat(caleDirector,"/");
-        strcat(caleDirector,citireDirector->d_name);
-
-        if((lstat(caleDirector,&informatiiFisier))==-1){
-            perror("Eroare de a lua informatii despre fisier!!!\n");
-            exit(-1);
-        }
-
-        strftime(timpModificare, sizeof(timpModificare), "%Y-%m-%d %H:%M:%S", localtime(&informatiiFisier.st_mtime));
-
-        if(S_ISDIR(informatiiFisier.st_mode)){
-            DIR *p=NULL;
-            if((p=opendir(citireDirector->d_name))==NULL){
-                perror("Eroare deschidere director2!!!\n");
-                exit(-1);
-            }
-
-            fprintf(fisier, "%s (%s)\n", caleDirector, timpModificare);
-
-            verificareAcelasiContinut(caleDirector,p,fisier);
-            closedir(p);
-        }
-        else if(S_ISREG(informatiiFisier.st_mode)){
-            fprintf(fisier, "%s (%s)\n", caleDirector, timpModificare);  
-        }
-
-        free(caleDirector);
-    }
-}
-
-int main(int argc , char **argv){
-    char *director=argv[1];
-
-    printf("%s\n",argv[1]);
-
-    DIR *p=opendir(director);
-    if(p==NULL){
-        perror("Eroare deschidere director!!!\n");
-        exit(-1);
-    }
-    FILE *fisierNou = fopen("fisierNou.txt","w");
-    if (fisierNou == NULL) {
-        perror("Error opening new output file");
-        exit(-1);
-    }
-
-    // Traverse directories and files and write to new file
-    verificareAcelasiContinut(director, p, fisierNou);
-
-    // Close the directory and new file
-    closedir(p);
-    fclose(fisierNou);
-    return 0;
-}
-*/
-
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -90,7 +9,7 @@ int main(int argc , char **argv){
 #include <time.h>
 #include <errno.h>
 
-void citesteDirector(DIR* director, char* cale){
+void citesteDirector(DIR* director, char* cale,int fd){
     struct dirent* aux;
     struct stat infFisier;
 
@@ -110,10 +29,14 @@ void citesteDirector(DIR* director, char* cale){
 
                 d = opendir(nume);
                 if(!d){
-                    fprintf(stderr,"Nu am putut deschide un director interior.\n");
+                    perror("Nu am putut deschide un director interior.\n");
                     exit(-1);
                 }
-                citesteDirector(d,nume);
+                citesteDirector(d,nume,fd);
+
+                dprintf(fd,"%s : %s : %ld\n",cale,aux->d_name,infFisier.st_mtime);
+
+                free(nume);
             }
             else if(aux->d_type == DT_REG){
                 char* nume = malloc(sizeof(char)*(strlen(cale)+1+strlen(aux->d_name)));
@@ -125,27 +48,38 @@ void citesteDirector(DIR* director, char* cale){
                     perror("Eroare de a lua informatii despre fisier!!!\n");
                     exit(-1);
                 }
+
+                dprintf(fd,"%s : %s : %ld\n",cale,aux->d_name,infFisier.st_mtime);
+
+                free(nume);
             }
-            printf("%s : %s : %ld\n",cale,aux->d_name,infFisier.st_mtime);
         }
-    }
-    if(errno != 0){
-        fprintf(stderr,"Eroare la citirea din director.\n");
-        exit(-1);
     }
 }
 
 int main(int argc, char* argv[]){
 
+    int fd =0;
+    struct dirent* aux;
+    struct stat infFisier;
+
     for(int i=1;i<argc;i++){
-        printf("Verificare director nr. : %d\n",i);
+        fd = open("temporary.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd == -1) { 
+            perror("Eroare deschidere fisier temporar!!!");
+            exit(-1);
+        }
+        printf("Verificare director : %s\n",argv[i]);
         DIR* director;
         if(!(director = opendir(argv[i]))){
-            fprintf(stderr,"Calea catre director nu este corecta/directorul nu s-a putut deschide.\n");
+            perror("Calea catre director nu este corecta/directorul nu s-a putut deschide.\n");
             exit(-1);
         }
 
-        citesteDirector(director,argv[i]);
+        citesteDirector(director,argv[i],fd);
+
+        close(fd);
+
     }
 
     return 0;
